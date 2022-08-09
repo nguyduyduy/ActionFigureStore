91adb372800467ed1568772a2accbb29599ae98c
nguyr <duymisc@gmail.com>
2022-08-09 03:14:05 -0500

    Spring boot application is running

---
+++ actionfigurestore/src/main/java/com/store/actionfigurestore/ActionFigureStoreApplication.java
@@ -0,0 +1,13 @@
+package com.store.actionfigurestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionFigureStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionFigureStoreApplication.class, args);
+	}
+
+}
